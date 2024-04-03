250342ba207c2f4866bf89202f46c40d817fd7c1
fawadaslam77 <fwadaslam77@gmail.com>
2024-04-04 00:29:59 +0500

    first commit

---
+++ app/dashboard/customers/create/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = {}}) => {
+  return (
+    <div>
+      New invoices
+    </div>
+  )
+}
+
+export default page
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>
+            Invocies Page
+        </div>
+    )
+}
+
+export default page
