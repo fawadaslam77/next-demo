2e08fa7cfb4a83bc871814d84e38ab043f902956
fawadaslam77 <fwadaslam77@gmail.com>
2024-04-04 05:12:21 +0500

    Add invoices table

---
+++ app/dashboard/invoices/create/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>
+            Create invoice
+        </div>
+    )
+}
+
+export default page
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,14 +1,30 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { usePathname, useSearchParams, useRouter } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+
+  const searchParams = useSearchParams();
+  const { replace } = useRouter()
+  const pathname = usePathname()
+  const handleSearch = (val: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (val.trim()) {
+      params.set('query', val)
+    } else {
+      params.delete('query')
+    }
+    replace(`${pathname}?${params.toString()}`)
+  }
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
         Search
       </label>
       <input
+        defaultValue={searchParams.get('query')?.toString()}
+        onChange={(e) => { handleSearch(e.target.value) }}
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
       />
