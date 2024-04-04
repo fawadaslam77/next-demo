4e540d845914d72ec06cf5b74bbcf1922a58d39b
fawadaslam77 <fwadaslam77@gmail.com>
2024-04-05 02:15:22 +0500

    Add error pages

--- app/dashboard/invoices/[Id]/edit/page.tsx
+++ app/dashboard/invoices/[Id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
 
 
 export default async function Page({ params }: { params: { Id: string } }) {
@@ -9,6 +10,10 @@ export default async function Page({ params }: { params: { Id: string } }) {
         fetchInvoiceById(id),
         fetchCustomers(),
     ]);
+
+    if (!invoice) {
+        notFound();
+      }
     // console.log('Here')
     // // console.log(invoice)
     // console.log('End here')
